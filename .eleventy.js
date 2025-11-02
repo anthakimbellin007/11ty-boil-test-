6e9e47c1bf5c587b08958dd5491998e0c34b79bd
anthakimbellin007 <bellin45@msu.edu>
2025-11-02 23:31:32 +0000

    change

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-        eleventyConfig.addPassthroughCopy("src");
+       
 
 };
